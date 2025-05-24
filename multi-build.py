0d63f57a9f4d06149e085151ef96446d4cb7c77e
Nayjest <mail@vitaliy.in>
2025-05-24 19:25:37 +0200

    build system update

--- multi-build.py
+++ multi-build.py
@@ -2,22 +2,22 @@ import re
 from pathlib import Path
 import subprocess
 
-NAMES = (
-    ("lm-proxy", "LM-Proxy"),
-    ("llm-proxy-server", "LLM Proxy Server"),
-    ("ai-proxy-server", "AI Proxy Server"),
-    ("lm-proxy-server", "LM Proxy Server"),
-    ("openai-http-proxy", "OpenAI HTTP Proxy"),
-    ("inference-proxy", "Inference Proxy"),
-    ("oai-proxy", "OAI Proxy"),
-)
-FILES = (
+NAMES = [
+    ["lm-proxy", "LM-Proxy"],
+    ["llm-proxy-server", "LLM Proxy Server"],
+    ["ai-proxy-server", "AI Proxy Server"],
+    ["lm-proxy-server", "LM Proxy Server"],
+    ["openai-http-proxy", "OpenAI HTTP Proxy"],
+    ["inference-proxy", "Inference Proxy"],
+    ["oai-proxy", "OAI Proxy"],
+]
+FILES = [
     "pyproject.toml",
     "README.md",
-)
+]
 
 
-def replace_name(old_names: tuple, new_names: tuple, files: list[str] = None):
+def replace_name(old_names: list[str], new_names: list[str], files: list[str] = None):
     files = files or FILES
     for i in range(len(old_names)):
         old_name = old_names[i]
@@ -33,9 +33,12 @@ def replace_name(old_names: tuple, new_names: tuple, files: list[str] = None):
                 ), encoding="utf-8"
             )
 
+
+
 prev = NAMES[0]
-for n in NAMES[1:]+(NAMES[0]):
-    print(f"Building for project name: {n[0]}...")
-    replace_name(prev, n)
+for nxt in NAMES[1:]+[NAMES[0]]:
+    print(f"Building for project name: {nxt[0]}...")
+    replace_name(prev, nxt)
     subprocess.run(["poetry", "build"], check=True)
+    prev = nxt
 print("All builds completed.")
